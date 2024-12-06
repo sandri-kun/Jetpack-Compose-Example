31392c2c7940437ab4f59c96c2b12ad4e6df5255
Sandri Kun <sndriirsyd.kun@gmail.com>
2024-12-06 08:22:07 +0700

    main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
 
     val nav_version = "2.8.4"
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 }
